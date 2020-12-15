ae31b3e1ee7ec5217e49ec0a5efb2c4bde29c3d1
vmg999 <vmg999@gmail.com>
2020-12-16 00:44:15 +0200

    feat: config

--- covid/.eslintrc.js
+++ covid/.eslintrc.js
@@ -3,7 +3,8 @@ module.exports = {
         "browser": true,
         "es2021": true
     },
-    "extends": "eslint:recommended",
+    // "extends": "eslint:recommended",
+    "extends": "airbnb",
     "parserOptions": {
         "ecmaVersion": 12,
         "sourceType": "module"
---
+++ covid/webpack.config.js
@@ -0,0 +1,19 @@
+var path = require('path');
+const CopyWebpackPlugin= require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+      new CopyWebpackPlugin({
+          patterns: [
+            {
+            from: './src/index.html',
+            to: './index.html'
+            }
+      ]}),
+    ]
+};
